3b4e8d8ed59f3c69f201fb6e72cc6c57531d083b
Jake <jake.gads@gmail.com>
2026-01-14 18:57:44 -0500

    ready for testing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,15 +6,18 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.sub_containers.DriveBaseContainer;
 import frc.robot.subsystems.DashboardWriter;
+import frc.robot.subsystems.Intake;
 
 public class RobotContainer {
     // Sub-Containers
     public final DriveBaseContainer driveBaseContainer; // HINT: looking for DriveBase Controls look in here
     // Subsystems
     public final DashboardWriter dashboardWriter = new DashboardWriter();
+    public final Intake intake = new Intake();
 
 
     // Controllers
@@ -26,6 +29,12 @@ public class RobotContainer {
     }
 
     private void configureBindings() {
+        driverController.a()
+            .onTrue(
+                new InstantCommand(() -> intake.intake())
+            ).onFalse(
+             new InstantCommand(() -> intake.stop())
+            );
         
     }
 
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -11,20 +11,23 @@ public class Intake extends SubsystemBase {
   /** Creates a new Intake. */
   SparkMAXContainer motor;
   public final int intake_id = 1;
-  public final double set_speed = .5;
+  public final double defult_speed = .5;
 
   public Intake() {
     motor= new SparkMAXContainer(intake_id);
   }
   
   public void intake() {
-    motor.motor.set(set_speed);
+    motor.motor.set(defult_speed);
   }
 
   public void release() {
-    motor.motor.set(-set_speed);
+    motor.motor.set(-defult_speed);
   }
   
+  public void stop() {
+    motor.motor.set(0);
+  }
   @Override
   public void periodic() {
     motor.reportMotor("Intake");
