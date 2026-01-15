eacc6181f2c9c885c91e72edc0965479949d7e5c
MISTERMEGA910 <rsulpizio910@gmail.com>
2026-01-15 18:54:36 -0500

    done prototyping

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,15 +7,19 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.climber.ClimberRaise;
 import frc.robot.sub_containers.DriveBaseContainer;
+import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.DashboardWriter;
+import frc.robot.commands.climber.ClimberStop;
+
 
 public class RobotContainer {
     // Sub-Containers
     public final DriveBaseContainer driveBaseContainer; // HINT: looking for DriveBase Controls look in here
     // Subsystems
     public final DashboardWriter dashboardWriter = new DashboardWriter();
-
+    public final Climber climber = new Climber();
 
     // Controllers
     private final CommandXboxController driverController = new CommandXboxController(0);
@@ -26,7 +30,7 @@ public class RobotContainer {
     }
 
     private void configureBindings() {
-        
+                driverController.a().onTrue(new ClimberRaise(climber)).onFalse(new ClimberStop(climber));
     }
 
     public Command getAutonomousCommand() {
---
+++ src/main/java/frc/robot/commands/climber/ClimberStop.java
@@ -0,0 +1,41 @@
+package frc.robot.commands.climber;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Climber;
+
+public class ClimberStop extends Command{
+  /** Creates a new IntakeIntake. */
+  Climber climber;
+  boolean isDone = false;
+  public ClimberStop(Climber climber) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(climber);
+    this.climber = climber;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    climber.Stop();
+    System.out.println("Hey Rocco");
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    isDone = true;
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return isDone;
+  }
+}
+
+
+
