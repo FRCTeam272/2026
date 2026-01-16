59dc7e2ab6f3ffdc22dc7bf89b18a1effaff8b55
Jake Gads <jake.gads@gmail.com>
2026-01-15 19:01:06 -0500

    done prototipeing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,15 +7,17 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.shooter.ShooterShoot;
 import frc.robot.sub_containers.DriveBaseContainer;
 import frc.robot.subsystems.DashboardWriter;
+import frc.robot.subsystems.Shooter;
 
 public class RobotContainer {
     // Sub-Containers
     public final DriveBaseContainer driveBaseContainer; // HINT: looking for DriveBase Controls look in here
     // Subsystems
     public final DashboardWriter dashboardWriter = new DashboardWriter();
-
+    public final Shooter shooter = new Shooter();
 
     // Controllers
     private final CommandXboxController driverController = new CommandXboxController(0);
@@ -26,7 +28,7 @@ public class RobotContainer {
     }
 
     private void configureBindings() {
-        
+        driverController.rightTrigger().onTrue(new ShooterShoot(shooter, ()  -> .5)).onFalse(new ShooterShoot(shooter, () -> 0));
     }
 
     public Command getAutonomousCommand() {
