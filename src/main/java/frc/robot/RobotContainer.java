44b4d31d521dbc26e08ece3e36b98d7c2012ee1f
MISTERMEGA910 <rsulpizio910@gmail.com>
2026-01-22 18:09:31 -0500

    updated climber, untested

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,12 +7,12 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.climber.ClimberLower;
 import frc.robot.commands.climber.ClimberRaise;
 import frc.robot.sub_containers.DriveBaseContainer;
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.DashboardWriter;
 import frc.robot.commands.climber.ClimberStop;
-
 
 public class RobotContainer {
     // Sub-Containers
@@ -30,7 +30,8 @@ public class RobotContainer {
     }
 
     private void configureBindings() {
-                driverController.a().onTrue(new ClimberRaise(climber)).onFalse(new ClimberStop(climber));
+        driverController.a().whileTrue(new ClimberRaise(climber)).onFalse(new ClimberStop(climber));
+        driverController.b().whileTrue(new ClimberLower(climber)).onFalse(new ClimberStop(climber));
     }
 
     public Command getAutonomousCommand() {
