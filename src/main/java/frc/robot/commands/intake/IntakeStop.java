3ac34c587b43541e73c6513b44248432cf3795c5
Jake Gadaleta <jake.gads@gmail.com>
2026-02-11 18:34:41 -0500

    print remove

--- src/main/java/frc/robot/commands/intake/IntakeStop.java
+++ src/main/java/frc/robot/commands/intake/IntakeStop.java
@@ -25,7 +25,6 @@ public class IntakeStop extends Command {
   @Override
   public void execute() {
     intake.stop();
-    System.out.println("Hey Chloe");
   }
 
   // Called once the command ends or is interrupted.
