6e8b1da933640939d107907d953caf0687b46e00
Jake Gadaleta <jake.gads@gmail.com>
2026-02-12 18:27:07 -0500

    worked on controllers

---
+++ ControllerOutlines.md
@@ -0,0 +1,34 @@
+## Auto Align
+
+# Driver
+## Intakes Fuel
+- Left Trigger
+
+## Intake Retract
+- Y
+
+## Shoots Fuel
+- Right Trigger
+## SpinFlywheel
+- Right Trigger
+## Moves
+- Left X
+- Left Y
+- Right X
+
+# Operator
+
+## Agatate
+- Bottom Right Quadrant
+
+## TrimPots
+- Top Half
+### Shooter
+### Intake
+### Hood
+
+### Climber
+- Opposite of Agatae
+## Climb Raise 
+## Climb Lower
+## Climber 0
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import frc.robot.commands.drivetrain.AlignToHub;
+import frc.robot.commands.intake.IntakeCommands;
 import frc.robot.commands.intake.IntakeIntake;
 import frc.robot.commands.intake.IntakeStop;
 import frc.robot.commands.shooter.ShooterShoot;
@@ -25,6 +26,7 @@ import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Regulator;
 import frc.robot.subsystems.Shooter;
 import frc.tuner.FlyWheelAutoTuner;
+import frc.lib.controllers.LC_2026Custom;
 import frc.lib.utils.TrimPot;
 import frc.robot.commands.ComplexCommands;
 import frc.robot.commands.TrimPotCommands;
@@ -41,10 +43,10 @@ public class RobotContainer {
     public final Conveyor conveyor = new Conveyor();
 
     // Controllers
-    private final CommandXboxController driverController = new CommandXboxController(0);
-
+    private final CommandXboxController DC = new CommandXboxController(0);
+    private final LC_2026Custom OC = new LC_2026Custom(1);
     public RobotContainer() {
-        driveBaseContainer = new DriveBaseContainer(driverController);
+        driveBaseContainer = new DriveBaseContainer(DC);
         // configureBindings();
         configurTestBindings();
 
@@ -58,7 +60,7 @@ public class RobotContainer {
     }
 
     private void configurTestBindings() {
-        driverController.rightTrigger()
+        DC.rightTrigger()
                 .whileTrue(new InstantCommand(() -> {
                     conveyor.Load();
                     regulator.Load();
@@ -67,7 +69,7 @@ public class RobotContainer {
                     conveyor.Stop();
                     regulator.Stop();
                 }));
-        driverController.leftTrigger()
+        DC.leftTrigger()
                 .whileTrue(new InstantCommand(() -> {
                     intake.deploy();
                     // intake.intake();
@@ -75,76 +77,35 @@ public class RobotContainer {
                     // intake.stop();
                     intake.retract();
                 }));
-        driverController.y().onTrue(new InstantCommand(() -> shooter.SpinWheel(shooter.targetVelocity)));
-        driverController.b().onTrue(new InstantCommand(() -> shooter.SpinWheel(0)));
-        driverController.a().onTrue(new InstantCommand(() -> {
+        DC.y().onTrue(new InstantCommand(() -> shooter.SpinWheel(shooter.targetVelocity)));
+        DC.b().onTrue(new InstantCommand(() -> shooter.SpinWheel(0)));
+        DC.a().onTrue(new InstantCommand(() -> {
             shooter.AdjustHood(10);
         }));
-        driverController.x().onTrue(new InstantCommand(() -> {
+        DC.x().onTrue(new InstantCommand(() -> {
             shooter.AdjustHood(0);
         }));
     }
 
     private void configureBindings() {
-        // driverController.leftTrigger().onTrue(
-        // new AlignToHub(driveBaseContainer.drivetrain, driverController)
-        // ).onFalse(
-        // Commands.runOnce(() -> driveBaseContainer.driveHider())
-        // );
+        DC.leftTrigger()
+                .whileTrue(IntakeCommands.deployIntake(intake)).onFalse(new InstantCommand(() -> {
+                    intake.stop();
+                }));
+        DC.y().onTrue(IntakeCommands.retractIntake(intake));
+        DC.rightBumper().onTrue(
+            // @TODO: add auto update to shooter velocity targets
+            new InstantCommand(() -> {shooter.SpinWheel(shooter.targetVelocity);})
+        );
 
-        // real controls
-        // driverController.leftTrigger().onTrue(new
-        // AlignToHub(driveBaseContainer.drivetrain,
-        // driverController)).onFalse(Commands.runOnce(() ->
-        // driveBaseContainer.driveHider()));
-        // Intake - Driver Controler Left Bumper
-        // driverController.leftBumper().whileTrue(ComplexCommands.Intake(intake)).onFalse(ComplexCommands.StopIntake(intake));
-
-        // // // Stop Shooter - Left Bumper
-        // driverController.rightBumper().onTrue(new ShooterStop(shooter));
-        // // Shoot - Right Trigger
-        // driverController.rightTrigger().onTrue(new ShooterShoot(shooter, () ->
-        // shooter.targetVelocity));
-        // // Conveyor and Regulator - Driver A Button
-        // driverController.a().whileTrue(
-        // new InstantCommand(() -> this.regulator.Load())
-        // .alongWith(new InstantCommand(() -> this.conveyor.Load()))
-        // ).onFalse(
-        // new InstantCommand(() -> this.regulator.Stop()).alongWith(
-        // new InstantCommand(() -> this.conveyor.Stop())
-        // )
-        // );
-        // driverController.a().whileTrue(new InstantCommand(() ->
-        // conveyor.Load())).onFalse(new InstantCommand(() -> conveyor.Stop()));
-        // TEST CONTROLS
-        // driverController.rightTrigger().onTrue(new InstantCommand(() ->
-        // shooter.SpinWheel(shooter.targetVelocity)));
-        // driverController.y().onTrue(new InstantCommand(() -> shooter.SpinWheel(0)));
-        // driverController.a().whileTrue(new InstantCommand(() ->
-        // regulator.startAll())).onFalse(new InstantCommand(() ->
-        // regulator.stopAll()));
-        // driverController.b().onTrue(new Command() {
-        // @Override
-        // public void initialize() {
-        // super.initialize();
-        // // shooter.SpinWheel(0);
-        // regulator.stopAll();
-        // intake.stop();
-        // }
-        // // });
-        // driverController.leftTrigger().whileTrue(new Command() {
-        // @Override
-        // public void initialize() {
-        // super.initialize();
-        // intake.intake();
-        // // conveyor.Load();
-        // }
-        // }).onFalse(new InstantCommand(() -> intake.stop()));
-        // .andThen(new InstantCommand(() -> conveyor.Stop())));
-
-        // driverController.leftBumper().whileTrue(new InstantCommand(() ->
-        // intake.release())).onFalse(new InstantCommand(() -> intake.stop()));
-
+        OC.AgitateButton.whileTrue(IntakeCommands.hopperAgitation(intake));
+        OC.ShooterTrimPotUpButton.onTrue(new InstantCommand(() -> shooter.flywheelTrim.adjusterValue += 1));
+        OC.ShooterTrimPotDownButton.onTrue(new InstantCommand(() -> shooter.flywheelTrim.adjusterValue -= 1));
+        OC.IntakeTrimPotUpButton.onTrue(new InstantCommand(() -> intake.deployTrim.adjusterValue += 1));
+        OC.IntakeTrimPotDownButton.onTrue(new InstantCommand(() -> intake.deployTrim.adjusterValue -= 1));
+        OC.hoodTrimPotUpButton.onTrue(new InstantCommand(() -> shooter.hoodTrim.adjusterValue += 1));
+        OC.hoodTrimPotDownButton.onTrue(new InstantCommand(() -> shooter.hoodTrim.adjusterValue -= 1));
+        
     }
 
     public Command getAutonomousCommand() {
