ad400d42fdec6105853b59df8ab74c13780fba08
Jake Gadaleta <jake.gads@gmail.com>
2026-04-08 19:17:53 -0400

    vision subsystem updates

--- src/main/java/frc/robot/sub_containers/DriveBaseContainer.java
+++ src/main/java/frc/robot/sub_containers/DriveBaseContainer.java
@@ -21,10 +21,10 @@ import frc.robot.subsystems.Regulator;
 
 public class DriveBaseContainer {
     public AutoContainer autoContainer;
-    public static final double maxSpeedFactor = .85; // 85
-    public static final double intakeSpeedFactor = .85; // 32, same as
+    public static final double maxSpeedFactor = .25; // 85
+    public static final double intakeSpeedFactor = .25; // 32, same as
     public static double speedFactor = maxSpeedFactor;
-    public static double rotationFactor = .85; // 85
+    public static double rotationFactor = .25; // 85
     
     static {
         // edu.wpi.first.wpilibj.smartdashboard.SmartDashboard.putNumber("Speed Factor", speedFactor);
